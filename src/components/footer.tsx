b6a22138858430ee816bb317cb61cc8aa9cee47f
Alden Vallestero <aldenvallestero@icloud.com>
2022-04-22 15:40:38 +0800

    feat: include job email address

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,7 @@
 export default function Footer() {
 	return (
         <div className="container-fluid bg-light p-4">
+            <span className="d-block">aldenvallestero.job@gmail.com</span>
             <span className="d-block fw-bold">&copy; Alden Vallestero</span>
         </div>
 	);
